0077265f90eba268bc843af712e71c61951fb7f8
Gbolaga Famodun <gbolaga.famodun@gmail.com>
2024-11-17 05:09:05 -0500

    flyway db integration and migration

--- src/main/java/com/hiroyalty/reactive/customer/CustomerRepository.java
+++
@@ -1,4 +0,0 @@
-package com.hiroyalty.reactive.customer;
-
-public interface CustomerRepository {
-}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+create table customers(
+    id serial not null primary key,
+    firstname varchar(250),
+    lastname varchar(250),
+    age integer
+)
