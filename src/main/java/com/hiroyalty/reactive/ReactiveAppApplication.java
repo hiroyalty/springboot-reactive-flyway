2e35be4f54630f5683fed88145572dd994c9da83
Gbolaga Famodun <gbolaga.famodun@gmail.com>
2024-11-17 07:55:12 -0500

    update command line runner

--- src/main/java/com/hiroyalty/reactive/ReactiveAppApplication.java
+++ src/main/java/com/hiroyalty/reactive/ReactiveAppApplication.java
@@ -2,6 +2,7 @@ package com.hiroyalty.reactive;
 
 import com.github.javafaker.Faker;
 import com.hiroyalty.reactive.customer.Customer;
+import com.hiroyalty.reactive.customer.CustomerRequest;
 import com.hiroyalty.reactive.customer.CustomerService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,21 +16,20 @@ public class ReactiveAppApplication {
 		SpringApplication.run(ReactiveAppApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(CustomerService customerService) {
-//		return args -> {
-//			for (int i = 0; i < 100; i++) {
-//				Faker faker = new Faker();
-//				// note you have to subscribe to the save method to persist it in the database.
-//				customerService.save(
-//						Customer.builder()
-//								.firstname(faker.name().firstName())
-//								.lastname(faker.name().lastName())
-//								.age(faker.number().numberBetween(22, 45))
-//								.build()
-//				).subscribe();
-//			}
-//		};
-//	}
+	@Bean
+	public CommandLineRunner commandLineRunner(CustomerService customerService) {
+		return args -> {
+			for (int i = 0; i < 100; i++) {
+				Faker faker = new Faker();
+				// note you have to subscribe to the save method to persist it in the database.
+				CustomerRequest request = new CustomerRequest();
+				request.setFirstname(faker.name().firstName());
+				request.setLastname(faker.name().lastName());
+				request.setAge(faker.number().numberBetween(22, 45));
+				
+				customerService.save(request).subscribe();
+			}
+		};
+	}
 
 }
